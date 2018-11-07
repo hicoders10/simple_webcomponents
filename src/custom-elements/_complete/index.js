4ceda49d4b1b5440d40c45cf641a683cc983d28e
Albert Vàzquez <hola@albertvazquez.es>
2018-11-07 14:34:10 +0100

    progress and complete example

---
+++ src/custom-elements/_complete/index.js
@@ -0,0 +1,33 @@
+
+class MyButton extends HTMLElement {
+    constructor() {
+        super();
+
+        const templateContent = document.getElementById('element').content;
+
+        const clone = document.importNode(templateContent, true);
+
+        clone.querySelectorAll('button')[0].innerText = this.getAttribute('text');
+
+        this.append(clone);
+
+        /*this.innerText = this.getAttribute('text');
+        const div = document.createElement('div');
+        const btn = document.createElement('button');
+        btn.innerText = 'haz click';
+        div.appendChild(btn);
+        this.append(div)*/
+
+        this.addEventListener('mouseover', () => this.dispatchEvent(new CustomEvent('yeeh')))
+    }
+
+    connectedCallback() {
+        console.log('connected!');
+    }
+
+    disconnectedCallback() {
+        console.log('disconnected!');
+    }
+}
+
+window.customElements.define('my-button', MyButton);
